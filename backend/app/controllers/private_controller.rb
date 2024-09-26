631882b3128af5cfc50512c57fe6bd3bfa51856a
IsCathal <mccabemcathal@gmail.com>
2024-09-26 17:55:56 +0100

    feat: add devise JWT

---
+++ backend/app/controllers/private_controller.rb
@@ -0,0 +1,13 @@
+class PrivateController < ApplicationController
+  def test
+  end
+end
+# api/app/controllers/private_controller.rb
+class PrivateController < ApplicationController
+  before_action :authenticate_user!
+  def test
+    render json: {
+      message: "This is a secret message. You are seeing it because you have successfully logged in." 
+    }
+  end
+end
